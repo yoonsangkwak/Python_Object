c7fe382c8cff09edcb50a214ba5ce963bca59b53
yoonsangkwak <67727981+yoonsangkwak@users.noreply.github.com>
2020-08-02 14:17:50 +0900

    learning
    python class

---
+++ objet1.py
@@ -0,0 +1,20 @@
+class User:
+    def say_hello(some_user):
+        print(f"안녕하세요! 저는 {some_user.name}")
+
+
+user1 = User()
+user2 = User()
+user3 = User()
+
+user1.name = "김대위"
+user1.email = "captain@codeit.kr"
+user1.password = "12345"
+
+user2.name = "이회중"
+user2.email = "hoijoong@codeit.kr"
+user2.password = "34567"
+
+user3.name = "엄준식"
+user3.email = "isalive@codeit.kr"
+user3.password = "67875"
