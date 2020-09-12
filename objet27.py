1dbbd23be734287deafff00ecdbe7fc2891a458d
yoonsangkwak <yunsang08@naver.com>
2020-09-12 17:13:50 +0900

    decorator capsule

--- objet27.py
+++ objet27.py
@@ -5,7 +5,7 @@ class Citizen:
     def __init__(self, name, age, resident_id):
         """이름, 나이, 주민등록번호"""
         self.name = name
-        self.set_age(age)
+        self._age = age
         self._resident_id = resident_id
 
 
@@ -24,21 +24,22 @@ class Citizen:
         return self.name + "씨는 " + str(self._age) + "살입니다!"
 
 
-    def get_age(self):  # getter 메소드
-        """숨겨 놓은 인스턴스 변수 __age의 값을 받아오는 메소드"""
+    @property   # getter 메소드
+    def age(self):
+        property("나이를 리턴합니다.")
         return self._age
 
-    
-    def set_age(self, value):   # setter 메소드
-        """숨겨 놓은 인스턴스 변수 __age의 값을 설정하는 메소드"""
+
+    @age.setter # setter 메소드
+    def age(self, value):
+        print("나이를 설정합니다.")
         if value < 0:
             print("나이는 0보다 작을 수 없습니다. 기본 값 0으로 나이를 설정하겠습니다.")
-            self._age = 0
         else:
             self._age = value
 
 
 young = Citizen("younghoon kang", 15, "12345678")
-print(young.get_age())
-young.set_age(30)
-print(young.get_age())
+print(young.age)
+young.age = 30
+print(young.age)
