b2c29383a9fe255e4cb58a01001377438e6cf947
yoonsangkwak <yunsang08@naver.com>
2020-09-14 19:39:45 +0900

    inherit

--- objet33.py
+++ objet33.py
@@ -21,17 +21,38 @@ class Employee:
 
 class Cashier(Employee):
     raise_percentage = 1.05
+    burger_price = 4000
 
     def __init__(self, name, wage, number_sold):
         super().__init__(name, wage)
         self.number_sold = number_sold
+
+
+    def take_order(self, money_received):
+        """주문과 돈을 받고 거스름돈을 리턴한다."""
+        if Cashier.burger_price > money_received:
+            print("돈이 충분하지 않습니다. 돈을 다시 계산해서 주세요!")
+            return money_received
+        else:
+            self.number_sold += 1
+            change = money_received - Cashier.burger_price
+            return change
 
     
     def __str__(self):
         return Cashier.company_name + " 계산대 직원: " + self.name
 
 
-print(Cashier.mro())
 
 young = Cashier("강영훈", 8900, 4)
 young.raise_pay()
+print(young.wage)
+
+print(young.take_order(7000))
+print(young.take_order(3000))
+
+print(young.burger_price)
+print(Cashier.burger_price)
+
+print(young.number_sold)
+print(young)
