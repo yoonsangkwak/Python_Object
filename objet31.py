24d0b83a55d8baacb383218ed1cbe05e2e76afa2
yoonsangkwak <yunsang08@naver.com>
2020-09-14 17:03:22 +0900

    deliveryman class

--- objet31.py
+++ objet31.py
@@ -30,12 +30,49 @@ class Cashier:
         return Cashier.company_name + " 계산대 직원: " + self.name
 
 
-jiwoong = Cashier("최지웅", 8900, 0)
-jiwoong.raise_pay()
-print(jiwoong.wage)
-print(jiwoong.take_order(7000))
-print(jiwoong.take_order(3000))
-print(jiwoong.burger_price)
-print(Cashier.burger_price)
-print(jiwoong.number_sold)
-print(jiwoong)
+class DeliveryMan:
+    """배달원 클래스"""
+    company_name = "코드잇 버거"    # 가게 이름
+    raise_percentage = 1.03     # 시급 인상률
+
+    def __init__(self, name, wage, on_standby):
+        self.name = name    # 이름
+        self.wage = wage    # 시급
+        self.on_standby = on_standby    # 대기 상태
+
+
+    def raise_pay(self):
+        """시급을 인상한다"""
+        self.wage *= self.raise_percentage
+
+
+    def deliver(self, address):
+        """배달원이 대기 중이면 주어진 주소로 배달을 보내고 아니면 설명 메세지를 출력한다"""
+        if self.on_standby:
+            print(address + "로 배달 나갑니다!")
+            self.on_standby = False
+        else:
+            print("이미 배달하러 나갔습니다!")
+
+
+    def back(self):
+        """배달원을 복귀 처리한다"""
+        self.on_standby = True
+
+
+    def __str__(self):
+        return DeliveryMan.company_name + " 배달원: " + self.name
+
+
+taeho = DeliveryMan("성태호", 7000, True)
+
+taeho.raise_pay()
+print(taeho.wage)
+
+taeho.deliver("서울시 코드잇로 51 최고 건물 401호")
+taeho.deliver("서울시 코드잇로 51 최고 건물 402호")
+
+taeho.back()
+taeho.deliver("서울시 코드잇로 51 최고 건물 402호")
+
+print(taeho)
