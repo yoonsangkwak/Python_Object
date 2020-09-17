ebf4cc84c4b305d26cc44cfe7d7aafb6d9942c44
yoonsangkwak <yunsang08@naver.com>
2020-09-17 15:14:52 +0900

    isinstance

--- objet38.py
+++ objet38.py
@@ -37,6 +37,17 @@ class Circle:
         return f"반지름 {self.radius}인 원"
 
 
+class Cylinder:
+    """원통 클래스"""
+    def __init__(self, radius, height):
+        self.radius = radius
+        self.height = height
+
+    def __str__(self):
+        """원통의 정보를 문자열로 리턴하는 메소드"""
+        return f"밑면 반지름 {self.radius}, 높이 {self.height}인 원기둥"
+
+
 class Paint:
     """그림판 프로그램 클래스"""
     def __init__(self):
@@ -44,7 +55,10 @@ class Paint:
 
     def add_shape(self, shape):
         """그림판에 도형을 추가한다"""
-        self.shapes.append(shape)
+        if isinstance(shape, Circle) or isinstance(shape, Rectangle):
+            self.shapes.append(shape)
+        else:
+            print("넓이, 둘레를 구하는 메소드가 없는 도형은 추가할 수 없습니다!")    
 
     def total_area_of_shapes(self):
         """그림판에 있는 모든 도형의 넓이의 합을 구한다"""
@@ -62,12 +76,14 @@ class Paint:
         return res_str
 
 
+paint_program = Paint()
+cylinder = Cylinder(7, 4)
 rectangle = Rectangle(3, 7)
 circle = Circle(4)
 
-paint_program = Paint()
-paint_program.add_shape(rectangle)
+paint_program.add_shape(cylinder)
 paint_program.add_shape(circle)
+paint_program.add_shape(rectangle)
 
 print(paint_program.total_area_of_shapes())
-print(paint_program.total_perimeter_of_shapes())
+print(paint_program.total_perimeter_of_shapes())    # 에러
