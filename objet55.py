5da7793873521393a8bd1a906266cad1790ef38a
yoonsangkwak <yunsang08@naver.com>
2020-09-19 11:31:13 +0900

    dependency inversion

--- objet55.py
+++ objet55.py
@@ -1,24 +1,48 @@
-class Sword:
+from abc import ABC, abstractmethod
+
+class IWeapon(ABC):
+    """무기 클래스"""
+    @abstractmethod
+    def use_on(self, other_character):
+        pass
+
+
+class Sword(IWeapon):
     """검 클래스"""
     def __init__(self, damage):
         self.damage = damage
 
-    def slash(self, other_character):
+    def use_on(self, other_character):
         """검 사용 메소드"""
         other_character.get_damage(self.damage)
 
 
+class Gun(IWeapon):
+    """총 클래스"""
+    def __init__(self, damage, num_rounds):
+        self.damage = damage
+        self.num_rounds = num_rounds    # 총알 갯수
+
+    def use_on(self, other_character):
+        """총 사용 메소드"""
+        if self.num_rounds > 0:
+            other_character.get_damage(self.damage)
+            self.num_rounds -= 1
+        else:
+            print("총알이 없어 공격할 수 없습니다")
+
+
 class GameCharacter:
     """게임 캐릭터 클래스"""
-    def __init__(self, name, hp, sword: Sword):
+    def __init__(self, name, hp, weapon: IWeapon):
         self.name = name
         self.hp = hp
-        self.sword = sword
+        self.weapon = weapon
 
     def attack(self, other_character):
         """다른 유저를 공격하는 메소드"""
         if self.hp > 0:
-            self.sword.slash(other_character)
+            self.weapon.use_on(other_character)
         else:
             print(self.name + "님은 사망해서 공격할 수 없습니다.")
 
@@ -40,10 +64,10 @@ class GameCharacter:
 
 
 bad_sword = Sword(1)
-good_sword = Sword(100)
+gun = Gun(100, 10)
 
 game_character_1 = GameCharacter("홍길동", 100, bad_sword)
-game_character_2 = GameCharacter("아무개", 1000, good_sword)
+game_character_2 = GameCharacter("아무개", 1000, gun)
 
 game_character_1.attack(game_character_2)
 game_character_1.attack(game_character_2)
